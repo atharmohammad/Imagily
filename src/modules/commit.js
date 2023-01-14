0acddf6badb89bdc895a85c18b8d6dec3c72aa9a
Athar <mohd.rule123@gmail.com>
2023-01-15 00:25:45 +0530

    commit files

--- src/modules/commit.js
+++ src/modules/commit.js
@@ -1,13 +1,16 @@
-const { v4}  =  require('uuid');
+const {v4}  =  require('uuid');
 const {walk} = require('./saveImage');
 const path = require('path');
-const fs = require('fs').promises;
+const fspromises = require('fs').promises;
+const fs = require('fs');
+
 const addDir = './.imagily/add/';
+const commitDir  = './.imagily/commits/'
 
 const findAddedFiles = async() => {
     let files = await walk(addDir);
     const lastAdd = files[0];
-    const images = await fs.readFile(path.join('./',lastAdd),'utf8');
+    const images = await fspromises.readFile(path.join('./',lastAdd),'utf8');
     const imageArray = images.split(/\r?\n/);
     // console.log(imageArray)
     return imageArray;
@@ -16,7 +19,19 @@ const findAddedFiles = async() => {
 const commit = async() => {
     const id = v4();
     const files = await findAddedFiles();
+    fspromises.mkdir(path.join(commitDir,id),{ recursive: true }, (err) => {
+        if (err) throw err;
+    })
     /* iterate on files to create array for them to save */
+
+    files.map(async(file)=>{
+        const name = file.split('/').join('_');
+        var file = fs.createWriteStream(path.join(`${commitDir}/${id}`,name));
+        file.on('error', function(err) { /* error handling */ });
+        files.forEach(function(v) { file.write(v + '\n'); });
+        file.end();
+    })
+    
 }
 
 module.exports = commit;
--- tests/commit.test.js
+++ tests/commit.test.js
@@ -2,7 +2,7 @@ const init = require('../src/modules/version')
 const commit = require('../src/modules/commit')
 
 describe("Commit the image",()=>{
-    it("creates a .imagily folder if don't exists",async()=>{
+    it("add commit in the .commit folder",async()=>{
         await init();
         await commit();
         
